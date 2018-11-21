357b9e50ece56ffb0ea865e6d30dd879e48606a6
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2018-11-21 22:10:37 +0700

    Add files via upload

---
+++ minigames.py
@@ -0,0 +1,61 @@
+import os
+
+print("===============================")
+print("   DAFTAR GAME YANG TERSEDIA   ")
+print("===============================")
+print("1. Tebak Kata                  ")
+print("2. Tebak Angka                 ")
+print("3. Lempar Dadu                 ")
+print("4. Arah Petualangan            ")
+print("5. Gunting Batu Kertas         ")
+print("6. Cerita Maker                ")
+print("0. Keluar Dari Game            ")
+pilih=raw_input("Masukkan Pilihan : ")
+if (pilih == '1'):
+      os.system("python tebakkata.py")
+      print("Tool Akan Kembali Ke menu Awal Dalam 8 Detik")
+      os.system("sleep 8")
+      os.system("clear")
+      os.system("python2 minigames.py")   
+elif (pilih == '2'):
+      os.system("python tebakangka.py")
+      print("Tool Akan Kembali Ke menu Awal Dalam 8 Detik")
+      os.system("sleep 8")
+      os.system("clear")
+      os.system("python2 minigames.py")
+elif (pilih == '3'):
+      os.system("python lempardadu.py")
+      print("Tool Akan Kembali Ke menu Awal Dalam 8 Detik")
+      os.system("sleep 8")
+      os.system("clear")
+      os.system("python2 minigames.py")
+elif (pilih == '4'):
+      os.system("python arahpetualangan.py")
+      print("Tool Akan Kembali Ke menu Awal Dalam 8 Detik")
+      os.system("sleep 8")
+      os.system("clear")
+      os.system("python2 minigames.py")
+elif (pilih == '5'):
+      os.system("python guntingbatukertas.py")
+      os.system("sleep 2")
+      print("Tool Akan Kembali Ke menu Awal Dalam 8 Detik")
+      os.system("sleep 8")
+      os.system("clear")
+      os.system("python2 minigames.py")
+elif (pilih == '6'):
+      os.system("python ceritamaker.py")
+      print("Tool Akan Kembali Ke menu Awal Dalam 8 Detik")
+      os.system("sleep 8")
+      os.system("clear")
+      os.system("python2 minigames.py")
+elif (pilih == '0'):
+      os.system("clear")
+      os.system("figlet KELUAR")
+      os.system("exit")
+else :
+      print("Format Yang Anda Masukkan Salah")
+      print("Please Wait")
+      os.system("sleep 3")
+      os.system("clear")
+      os.system("python2 asd.py")
+      
